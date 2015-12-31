60f91e805a8ce5e748851e4ce3bba0ed050db6a9
徐刘根 <1573876303@qq.com>
2015-12-31 20:53:47 +0800

    add:添加spring的bean获取的工具类

---
+++ util/src/main/java/com/xuliugen/common/util/spring/SpringGetBeanUtil.java
@@ -0,0 +1,36 @@
+package com.xuliugen.common.util.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+
+import javax.inject.Named;
+
+/**
+ * 用于手动获取bean
+ * <p/>
+ * 需要在spring的配置文件中进行注解，对该工具类进行注解
+ * <bean id="springGetBeanUtil" class="com.xuliugen.common.util.spring.SpringGetBeanUtil"/>
+ * Created by xuliugen on 15/12/6.
+ */
+@Named
+public class SpringGetBeanUtil implements BeanFactoryAware {
+
+    private static BeanFactory beanFactory = null;
+
+    private static SpringGetBeanUtil springGetBeanUtil = null;
+
+    public static SpringGetBeanUtil getInstance() {
+        if (springGetBeanUtil == null)
+            springGetBeanUtil = (SpringGetBeanUtil) beanFactory.getBean("springGetBeanUtil");
+        return springGetBeanUtil;
+    }
+
+    public void setBeanFactory(BeanFactory factory) throws BeansException {
+        this.beanFactory = factory;
+    }
+
+    public Object getBean(String beanName) {
+        return beanFactory.getBean(beanName);
+    }
+}
